c83d3a63c42c75f7ba2ad5e41cae5222a5dda2a5
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-08 08:00:21 +0100

    fetch bug fixed on script page

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 // 1 create a movie array
 let movies = []
 const url = "https://striveschool-api.herokuapp.com/api/movies";
-const myToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYxMDYzMDUsImV4cCI6MTYzNzMxNTkwNX0.coJwGNfHZHba2EmsENjQ - KPY - ZzrDFCnx24XJdM4Cko "
-
+//const myToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYxMDYzMDUsImV4cCI6MTYzNzMxNTkwNX0.coJwGNfHZHba2EmsENjQ - KPY - ZzrDFCnx24XJdM4Cko "
+const headers = new Headers({
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYxMDYzMDUsImV4cCI6MTYzNzMxNTkwNX0.coJwGNfHZHba2EmsENjQ - KPY - ZzrDFCnx24XJdM4Cko ",
+    'Content-Type': 'application/JSON'
+})
 
 // 2 fetching data globally
 const moviesFromServer = async() => {
     try {
-        const res = await fetch(url, {
+        const res = await fetch("https://striveschool-api.herokuapp.com/api/movies", {
             headers: {
-                "Authorization": myToken,
-                'Content-Type': 'application/JSON'
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYzNTAwMTUsImV4cCI6MTYzNzU1OTYxNX0.n3EiY73kRn-XZI3-zHU12UBEbKbpl7ZID2ZxktYdFfc"
             }
         })
         if (!res.ok) {
             throw new Error(alert("bad request error!"));
         } else {
             const data = await res.json()
-            console.log(data)
-            movies = data
+            movies = await data
+
         }
     } catch (error) { console.log(error) }
 }
 
 moviesFromServer()
+console.log(movies)
+    // 3 create a filtered array on genre query
 
-// 3 create a filtered array on genre query
-// 4 insert a lik with ID movie
+// 4 insert a link with ID movie
 // 5 display movies on the page
