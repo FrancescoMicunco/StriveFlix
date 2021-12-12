fde180f39156677186d463c20f2b7a29be6350bc
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-12 07:04:44 +0100

    gitignore

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 // 1 create a movie array
 let movies = []
 const url = "https://striveschool-api.herokuapp.com/api/movies";
-//const myToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYxMDYzMDUsImV4cCI6MTYzNzMxNTkwNX0.coJwGNfHZHba2EmsENjQ - KPY - ZzrDFCnx24XJdM4Cko "
 const headers = new Headers({
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYxMDYzMDUsImV4cCI6MTYzNzMxNTkwNX0.coJwGNfHZHba2EmsENjQ - KPY - ZzrDFCnx24XJdM4Cko ",
+    "Authorization": "process.env.BEARER_KEY",
     'Content-Type': 'application/JSON'
 })
 
 // 2 fetching data globally
 const moviesFromServer = async() => {
     try {
-        const res = await fetch("https://striveschool-api.herokuapp.com/api/movies", {
+        const res = await fetch(url, {
             headers: {
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2MzYzNTAwMTUsImV4cCI6MTYzNzU1OTYxNX0.n3EiY73kRn-XZI3-zHU12UBEbKbpl7ZID2ZxktYdFfc"
+                "Authorization": "process.env.BEARER_KEY"
             }
         })
         if (!res.ok) {
@@ -26,8 +25,3 @@ const moviesFromServer = async() => {
 }
 
 moviesFromServer()
-console.log(movies)
-    // 3 create a filtered array on genre query
-
-// 4 insert a link with ID movie
-// 5 display movies on the page
